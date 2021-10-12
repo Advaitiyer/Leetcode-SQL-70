feed11ac88ed7cc6ea60a05f24bb03c553c521d5
Advait Ramesh Iyer <48394731+Advaitiyer@users.noreply.github.com>
2021-10-12 17:59:59 -0400

    Update solution2.sql

--- solution2.sql
+++ solution2.sql
@@ -1,4 +1,10 @@
 /* Problem 2: Consecutive Available Seats
+Several friends at a cinema ticket office would like to reserve consecutive available seats.
+Can you help to query all the consecutive available seats order by the seat_id using the following cinema table?
+Note:
+The seat_id is an auto increment int, and free is bool ('1' means free, and '0' means occupied.).
+Consecutive available seats are more than 2(inclusive) seats consecutively available.
+
 Input Table: cinema (seat_id, free ("true", "false"))
 Output Table: seat_id
 */
